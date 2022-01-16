294b2635813e4fe806f0f7d715c3f7d675837f68
Daniel Sasser <desasser79@gmail.com>
2022-01-16 11:40:50 -0800

    band-aid for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="page-wrapper">
         {/* <Header /> */}
         <main>
-          <img src="/images/under_construction.jpg" />
+          <div></div>
         </main>
         {/* <Footer /> */}
       </div>
