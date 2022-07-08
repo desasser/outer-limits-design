592d074822d0d1e21398d6b3ab9a07242e14e7c9
desasser <desasser79@gmail.com>
2022-07-07 22:08:29 -0700

    styling nav

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,25 +11,27 @@ const About = () => {
       <div className="page-wrapper">
         <Header />
         <main>
-          <h2>About Outer Limits Designs</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-            consequatur nemo. Numquam, iste. Ducimus, saepe. Nobis recusandae
-            eligendi iusto est sequi distinctio error eveniet, unde reiciendis,
-            voluptates cumque quas tempore.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-            consequatur nemo. Numquam, iste. Ducimus, saepe. Nobis recusandae
-            eligendi iusto est sequi distinctio error eveniet, unde reiciendis,
-            voluptates cumque quas tempore.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-            consequatur nemo. Numquam, iste. Ducimus, saepe. Nobis recusandae
-            eligendi iusto est sequi distinctio error eveniet, unde reiciendis,
-            voluptates cumque quas tempore.
-          </p>
+          <section className="about-wrapper">
+            <h2>About Outer Limits Designs</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              consequatur nemo. Numquam, iste. Ducimus, saepe. Nobis recusandae
+              eligendi iusto est sequi distinctio error eveniet, unde reiciendis,
+              voluptates cumque quas tempore.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              consequatur nemo. Numquam, iste. Ducimus, saepe. Nobis recusandae
+              eligendi iusto est sequi distinctio error eveniet, unde reiciendis,
+              voluptates cumque quas tempore.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+              consequatur nemo. Numquam, iste. Ducimus, saepe. Nobis recusandae
+              eligendi iusto est sequi distinctio error eveniet, unde reiciendis,
+              voluptates cumque quas tempore.
+            </p>
+          </section>
         </main>
         <Footer />
       </div>
